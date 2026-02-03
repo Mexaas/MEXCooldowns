f8c40c00a580fd383a75bfa0fd3444bb319f1054
Mexaas <ruslancernaso@gmail.com>
2026-02-03 20:10:52 +0500

    Release 1.0.2

--- src/main/java/org/axc/mexcooldowns/Commands/reloadCommand.java
+++ src/main/java/org/axc/mexcooldowns/Commands/reloadCommand.java
@@ -27,7 +27,7 @@ public class reloadCommand implements CommandExecutor {
                 player.sendMessage(prefixComponent.append(adapter.parseMessage(Mexcooldowns.getInstance().getConfig().getString("messages.no-permission"))));
                 return true;
             }
-            player.sendMessage("\n               &x&6&5&5&F&D&BM&x&6&7&5&F&D&6E&x&6&9&6&0&D&2X&x&6&B&6&0&C&DC&x&6&D&6&0&C&8o&x&6&F&6&1&C&3o&x&7&0&6&1&B&Fl&x&7&2&6&2&B&Ad&x&7&4&6&2&B&5o&x&7&6&6&2&B&0w&x&7&8&6&3&A&Cn&x&7&A&6&3&A&7s\n\n&7  /mexcooldowns reload &8[Plugin reload]\n ");
+            player.sendMessage(adapter.parseMessage("\n               &x&6&5&5&F&D&BM&x&6&7&5&F&D&6E&x&6&9&6&0&D&2X&x&6&B&6&0&C&DC&x&6&D&6&0&C&8o&x&6&F&6&1&C&3o&x&7&0&6&1&B&Fl&x&7&2&6&2&B&Ad&x&7&4&6&2&B&5o&x&7&6&6&2&B&0w&x&7&8&6&3&A&Cn&x&7&A&6&3&A&7s\n\n&7  /mexcooldowns reload &8[Plugin reload]\n "));
         } else if (commandSender instanceof ConsoleCommandSender) {
             Mexcooldowns.getInstance().reloadConfig();
             return true;
